e241965dafc0b154ecf43649e9eaa481ada2b41a
minshallw12 <111906475+minshallw12@users.noreply.github.com>
2023-03-04 11:44:31 -0700

    found an api

---
+++ app.js
@@ -0,0 +1,14 @@
+// api docs - > https://api-ninjas.com/api/mortgagecalculator
+
+// $.ajax({
+//     method: 'GET',
+//     url: 'https://api.api-ninjas.com/v1/mortgagecalculator?loan_amount=200000&interest_rate=3.5&duration_years=30'
+//     headers: { 'X-Api-Key': 'YOUR_API_KEY'},
+//     contentType: 'application/json',
+//     success: function(result) {
+//         console.log(result);
+//     },
+//     error: function ajaxError(jqXHR) {
+//         console.error('Error: ', jqXHR.responseText);
+//     }
+// });
