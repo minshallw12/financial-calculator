1b12469faf03dfe928e846df57fcf7428a472b65
minshallw12 <111906475+minshallw12@users.noreply.github.com>
2023-03-09 15:35:36 -0700

    added amortization functionality

--- app.js
+++ app.js
@@ -2,25 +2,45 @@ document.querySelector('.button').addEventListener('click', calculate_amortizati
 
 
 function calculate_amortization() {
+
+    amount = document.querySelector('#amount_input').value;
+    term = document.querySelector('#term_input').value;
+    rate = document.querySelector('#rate_input').value;
+    date = document.querySelector('#date_input').value;
+ 
     axios
-        .get("https://www.commercialloandirect.com/monthlyPaymentAPI.php?pv=250000&rate=6&nperiod=30&io=0&pf=12&cf=1&pt=0&mode=json")
+        .get(`https://www.commercialloandirect.com/monthlyPaymentAPI.php?pv=${amount}&rate=${rate}&nperiod=${term}&io=0&pf=12&cf=1&pt=0&mode=json`)
         .then((response) => {
         console.log(response)
         let summary = response.data[0];
         let amortization_schedule = response.data[1];
+        
         let num_of_payments = summary['Number of Payments'];
         let monthly_payment = summary['Principal + Interest Payment'];
         let total_loan_cost = summary['Total Payments'];
-        let total_interest_paid = amortization_schedule['29']['Interest Paid'];
-        document.querySelector('#payment').innerHTML = `$${monthly_payment}`;
-        document.querySelector('#interest').innerHTML = `$${monthly_payment}`;
-        document.querySelector('#cost').innerHTML = `$${monthly_payment}`;
+        let total_interest_paid = amortization_schedule[amortization_schedule.length-1]['Interest Paid'];
+        document.querySelector('#payment').innerHTML = `$${monthly_payment.toFixed(2)}`;
+        document.querySelector('#interest').innerHTML = `$${total_interest_paid.toFixed(2)}`;
+        document.querySelector('#cost').innerHTML = `$${total_loan_cost.toFixed(2)}`;
         document.querySelector('#date').innerHTML = 'date here';
         document.querySelector('#number_of_payments').innerHTML = `${num_of_payments}`;
 
-        // console.log(total_interest_paid)
-        // console.log(summary)
-        // console.log(summary['Interest Only Payment'])
+
+        let my_list = document.querySelector('#my_list')    
+        //console.log(amortization_schedule)
+        for (let year=0; year<amortization_schedule.length; year++) {
+            console.log(amortization_schedule[year])
+            let ul = document.createElement("ul")
+
+            let balance = amortization_schedule[year]['Balance'];
+            let interest_paid = amortization_schedule[year]['Interest Paid'];
+            let principal = amortization_schedule[year]['Principal']
+
+            ul.innerText = `Year: ${year}  Balance: ${balance}  Total Interest Paid: ${interest_paid}  Applied Principal: ${principal}`;
+            my_list.appendChild(ul)
+        }
+
+
     })
     .catch((error) => {
         console.log("no good 1: ", error);
